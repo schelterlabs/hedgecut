b2630b7f0723df92ec88ee80d3644be03a1d3c5c
ssc <ssc@apache.org>
2020-06-28 14:22:59 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ impl SplitCandidate<'_> {
 }
 
 struct TitanicDataset {
+    pub num_records: u32,
     pub age: Vec<u8>,
     pub fare: Vec<u8>,
     pub siblings: Vec<u8>,
@@ -29,62 +30,77 @@ struct TitanicDataset {
 
 impl TitanicDataset {
 
+    pub fn from_csv() -> TitanicDataset {
+        let num_records = 886;
+
+        let mut age: Vec<u8> = Vec::with_capacity(num_records);
+        let mut fare: Vec<u8> = Vec::with_capacity(num_records);
+        let mut siblings: Vec<u8> = Vec::with_capacity(num_records);
+        let mut children: Vec<u8> = Vec::with_capacity(num_records);
+        let mut gender: Vec<u8> = Vec::with_capacity(num_records);
+        let mut pclass: Vec<u8> = Vec::with_capacity(num_records);
+
+        let mut labels: Vec<bool> = Vec::with_capacity(num_records);
+
+        let mut reader = csv::ReaderBuilder::new()
+            .has_headers(true)
+            .delimiter(b'\t')
+            .from_path("titanic-attributes.csv")
+            .unwrap();
+
+        for result in reader.records() {
+            let record = result.unwrap();
+
+            let record_id: u32 = u32::from_str(record.get(0).unwrap()).unwrap();
+            let attribute_name = record.get(1).unwrap();
+            let attribute_value = u8::from_str(record.get(2).unwrap()).unwrap();
+
+            match attribute_name {
+                "age" => age.insert(record_id as usize, attribute_value),
+                "fare" => fare.insert(record_id as usize, attribute_value),
+                "siblings" => siblings.insert(record_id as usize, attribute_value),
+                "children" => children.insert(record_id as usize, attribute_value),
+                "gender" => gender.insert(record_id as usize, attribute_value),
+                "pclass" => pclass.insert(record_id as usize, attribute_value),
+                "label" => labels.insert(record_id as usize, attribute_value == 1),
+
+                _ => println!("UNKNOWN ATTRIBUTE ENCOUNTERED")
+            }
+        }
+
+        TitanicDataset {
+            num_records: num_records as u32,
+            age,
+            fare,
+            siblings,
+            children,
+            gender,
+            pclass,
+            labels
+        }
+    }
+
 }
 
 
 fn main() {
 
-    let num_records = 886;
+    let dataset = TitanicDataset::from_csv();
 
-    let mut age: Vec<u8> = Vec::with_capacity(num_records);
-    let mut fare: Vec<u8> = Vec::with_capacity(num_records);
-    let mut siblings: Vec<u8> = Vec::with_capacity(num_records);
-    let mut children: Vec<u8> = Vec::with_capacity(num_records);
-    let mut gender: Vec<u8> = Vec::with_capacity(num_records);
-    let mut pclass: Vec<u8> = Vec::with_capacity(num_records);
-
-    let mut labels: Vec<bool> = Vec::with_capacity(num_records);
-
-    let mut reader = csv::ReaderBuilder::new()
-        .has_headers(true)
-        .delimiter(b'\t')
-        .from_path("titanic-attributes.csv")
-        .unwrap();
-
-    for result in reader.records() {
-        let record = result.unwrap();
-
-        let record_id: u32 = u32::from_str(record.get(0).unwrap()).unwrap();
-        let attribute_name = record.get(1).unwrap();
-        let attribute_value = u8::from_str(record.get(2).unwrap()).unwrap();
-
-        match attribute_name {
-            "age" => age.insert(record_id as usize, attribute_value),
-            "fare" => fare.insert(record_id as usize, attribute_value),
-            "siblings" => siblings.insert(record_id as usize, attribute_value),
-            "children" => children.insert(record_id as usize, attribute_value),
-            "gender" => gender.insert(record_id as usize, attribute_value),
-            "pclass" => pclass.insert(record_id as usize, attribute_value),
-            "label" => labels.insert(record_id as usize, attribute_value == 1),
-
-            _ => println!("UNKNOWN ATTRIBUTE ENCOUNTERED")
-        }
-    }
-
-    let mut record_ids_to_split: Vec<u32> = Vec::with_capacity(num_records);
-    for record_id in 0 .. num_records {
-        record_ids_to_split.push(record_id as u32);
+    let mut record_ids_to_split: Vec<u32> = Vec::with_capacity(dataset.num_records as usize);
+    for record_id in 0 .. dataset.num_records {
+        record_ids_to_split.push(record_id);
     }
 
     let split_candidates = vec![
-        SplitCandidate::new(&age, 5),
-        SplitCandidate::new(&fare, 12),
-        SplitCandidate::new(&children, 3),
-        SplitCandidate::new(&gender, 1),
+        SplitCandidate::new(&dataset.age, 5),
+        SplitCandidate::new(&dataset.fare, 12),
+        SplitCandidate::new(&dataset.children, 3),
+        SplitCandidate::new(&dataset.gender, 1),
     ];
 
     let split_stats: Vec<SplitStats> = split_candidates.iter()
-        .map(|candidate| compute_split_stats(&record_ids_to_split, &candidate, &labels))
+        .map(|candidate| compute_split_stats(&record_ids_to_split, &candidate, &dataset.labels))
         .collect();
 
     let maybe_best_split_stats = split_stats.iter().enumerate()
